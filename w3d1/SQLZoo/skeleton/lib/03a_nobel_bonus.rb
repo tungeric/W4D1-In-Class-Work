9137321caa76eebc7739dff2b34f2ccf3b71060a
drstonebraker <drew.stonebraker@gmail.com>
2017-08-14 17:39:36 -0700

    Finish nobel bonus

--- w3d1/SQLZoo/skeleton/lib/03a_nobel_bonus.rb
+++ w3d1/SQLZoo/skeleton/lib/03a_nobel_bonus.rb
@@ -14,5 +14,27 @@ require_relative './sqlzoo.rb'
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT DISTINCT
+      physics_years.yr
+    FROM (
+      SELECT
+        yr
+      FROM
+        nobels
+      WHERE
+        subject = 'Physics'
+    ) physics_years
+    LEFT JOIN (
+      SELECT
+        yr
+      FROM
+        nobels
+      WHERE
+        subject = 'Chemistry'
+    ) chemistry_years
+    ON
+      physics_years.yr = chemistry_years.yr
+    WHERE
+      chemistry_years.yr IS NULL
   SQL
 end
