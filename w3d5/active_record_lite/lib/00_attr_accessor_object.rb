2fb0e556481d6e261b38a6a3e82763d66e53a702
drstonebraker <drew.stonebraker@gmail.com>
2017-08-18 10:53:01 -0700

    Write attr_accessor method

--- w3d5/active_record_lite/lib/00_attr_accessor_object.rb
+++ w3d5/active_record_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) do
+        instance_variable_get("@#{name}")
+      end
+
+      define_method("#{name}=") do |new_val|
+        instance_variable_set("@#{name}", new_val)
+      end
+    end
   end
 end
