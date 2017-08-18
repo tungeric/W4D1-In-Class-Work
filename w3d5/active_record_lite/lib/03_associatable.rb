bb493a78afc49120c6f90f9149e772558edf6456
drstonebraker <drew.stonebraker@gmail.com>
2017-08-18 16:31:17 -0700

    Refactor initialize methods

--- w3d5/active_record_lite/lib/03_associatable.rb
+++ w3d5/active_record_lite/lib/03_associatable.rb
@@ -21,21 +21,33 @@ end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    @foreign_key = options[:foreign_key] ||
-      "#{name.to_s.underscore}_id".to_sym
-    @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] ||
-      name.to_s.camelize
+    defaults = {
+      foreign_key: "#{name.to_s.underscore}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.camelize
+    }
+
+    options = defaults.merge(options)
+
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+    @class_name = options[:class_name]
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    @foreign_key = options[:foreign_key] ||
-      "#{self_class_name.to_s.underscore}_id".to_sym
-    @primary_key = options[:primary_key] || :id
-    @class_name = options[:class_name] ||
-      name.to_s.camelize.singularize
+    defaults = {
+      foreign_key: "#{self_class_name.to_s.underscore}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.camelize.singularize
+    }
+
+    options = defaults.merge(options)
+
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+    @class_name = options[:class_name]
   end
 end
 
