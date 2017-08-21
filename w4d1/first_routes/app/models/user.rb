55682713b2635972add062c05cfd8a2a1acdb9f2
drstonebraker <drew.stonebraker@gmail.com>
2017-08-21 15:29:08 -0700

    Setup tables, associations, and validations

--- w4d1/first_routes/app/models/user.rb
+++ w4d1/first_routes/app/models/user.rb
@@ -1,2 +1,35 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  username   :string           not null
+#
+
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+
+  has_many :viewed_artwork_shares,
+    primary_key: :id,
+    foreign_key: :viewer_id,
+    class_name: :ArtworkShare
+
+  has_many :artworks,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: :Artwork
+
+  has_many :viewed_artworks,
+    through: :viewed_artwork_shares,
+    source: :artwork
+
+  has_many :viewed_artists,
+    through: :viewed_artworks,
+    source: :artist
+
+  has_many :viewers,
+    through: :artworks,
+    source: :shared_viewers
 end
